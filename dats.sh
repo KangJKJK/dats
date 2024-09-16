a6e903bbd9d4f7f831c4c41b723d25400df04486
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-09-16 23:40:11 +0900

    Update dats.sh

--- dats.sh
+++ dats.sh
@@ -8,107 +8,77 @@ YELLOW='\033[0;33m'
 CYAN='\033[36m'
 NC='\033[0m' # No Color
 
-echo -e "${GREEN}Pingpong 노드 설치 및 설정 스크립트입니다.${NC}"
+echo -e "${GREEN}Dats 노드 설치 및 설정 스크립트입니다.${NC}"
 echo ""
 
-# 사용자에게 선택지를 제공
-echo -e "${BOLD}${CYAN}1. Pingpong Node 설치${NC}"
-echo -e "${BOLD}${CYAN}2. Pingpong Depin 설정${NC}"
-echo -e "${YELLOW}1번을 선택하여 새로 설치할시 로그가 계속 나오기때문에 컨트롤A+D로 스크립트를 종료해주세요.${NC}"
-echo -e "${YELLOW}컨트롤A+D로 종료후 스크립트를 재실행하여 2번을 선택하고 Depin 설정을 마무리해주세요.${NC}"
-read -p "원하는 작업의 번호를 입력하세요: " CHOICE
-
-if [[ "$CHOICE" -eq 1 ]]; then
-    # Pingpong Node 설치 단계
-    echo -e "${GREEN}Pingpong 노드 설치를 시작합니다.${NC}"
-    echo ""
-
-    command_exists() {
-        command -v "$1" &> /dev/null
-    }
-
-    # 도커 설치 확인
-    echo -e "${BOLD}${CYAN}Docker 설치 확인 중...${NC}"
-    if ! command_exists docker; then
-        echo -e "${RED}Docker가 설치되어 있지 않습니다. Docker를 설치하는 중입니다...${NC}"
-        sudo apt update && sudo apt install -y curl net-tools
-        curl -fsSL https://get.docker.com -o get-docker.sh
-        sudo sh get-docker.sh
-        echo -e "${GREEN}Docker가 성공적으로 설치되었습니다.${NC}"
-    else
-        echo -e "${GREEN}Docker가 이미 설치되어 있습니다.${NC}"
-    fi
-
-    # pingpong 패키지 설치
-    echo -e "${BOLD}${CYAN}pingpong 패키지 설치 중...${NC}"
-    wget https://pingpong-build.s3.ap-southeast-1.amazonaws.com/linux/latest/PINGPONG
-
-    # pingpong 노드 설정 안내
-    echo -e "${BOLD}${CYAN}pingpong 노드를 설정 중...${NC}"
-    echo -e "${YELLOW}해당 사이트에 방문하세요${NC}"
-    echo -e "${YELLOW}https://app.pingpong.build/mining/devices${NC}"
-    echo -e "${YELLOW}Add device를 누른 후 Device key를 복사하세요.${NC}"
-
-    # 사용자로부터 Device key를 입력받음
-    read -p "Device key를 입력하세요: " DEVICE_KEY
-
-    # 패키지에 실행 권한을 부여하고 실행
-    chmod +x ./PINGPONG
-    ./PINGPONG --key "$DEVICE_KEY"
-
-elif [[ "$CHOICE" -eq 2 ]]; then
-    # Pingpong Depin 설정 단계
-    echo -e "${GREEN}${CYAN}Pingpong Depin 설정을 시작합니다.${NC}"
-    echo ""
-
-    echo -e "${BOLD}1.0G 노드 설정 중...${NC}"
-    read -p "EVM 지갑 프라이빗키를 입력하세요 (0G 노드용): " OGPRIVATE_KEY
-    ./PINGPONG config set --0g="$OGPRIVATE_KEY"
-    ./PINGPONG stop --depins=0g
-    ./PINGPONG start --depins=0g
-    echo ""
-
-    echo -e "${BOLD}2. AIOZ 노드 설정 중...${NC}"
-    curl -LO https://github.com/AIOZNetwork/aioz-dcdn-cli-node/files/13561211/aioznode-linux-amd64-1.1.0.tar.gz
-    tar xzf aioznode-linux-amd64-1.1.0.tar.gz
-    mv aioznode-linux-amd64-1.1.0 aioznode
-    ./aioznode keytool new --save-priv-key privkey.json
-    echo -e "${BOLD}${CYAN}privkey.json 파일의 내용은 다음과 같습니다:${NC}"
-    cat privkey.json
-    echo ""
-    read -p "privkey.json 안에서 개인키로 보이는 내용만 복사해서 적으세요 (\"\"는 제외): " OZPRIVATE_KEY
-    ./PINGPONG config set --aioz="$OZPRIVATE_KEY"
-    ./PINGPONG stop --depins=aioz
-    ./PINGPONG start --depins=aioz
-    echo ""
-
-    echo -e "${BOLD}3. Grass 노드 설정 중...${NC}"
-    echo -e "${YELLOW}해당 사이트로 이동하세요: https://app.getgrass.io/dashboard${NC}"
-    echo -e "${YELLOW}F12를 누른 후 상단 메뉴바에서 애플리케이션을 클릭하세요${NC}"
-    echo -e "${YELLOW}왼쪽 카테고리바에서 저장용량 - 로컬스토리지를 클릭하세요${NC}"
-    read -p "userID라고 표기된 구문을 복사해서 적으세요 (\"\"는 제외): " GRASSID
-    ./PINGPONG config set --grass.access="$GRASSID" --grass.refresh="$GRASSID"
-    ./PINGPONG stop --depins=grass
-    ./PINGPONG start --depins=grass
-    echo ""
-
-    echo -e "${BOLD}4. BlockMesh 노드 설정 중...${NC}"
-    echo -e "${YELLOW}해당 사이트로 이동하여: https://app.blockmesh.xyz/register?invite_code=kangjk${NC}"
-    read -p "회원가입을 진행한 Email을 적으세요: " BMEMAIL
-    read -p "패스워드를 입력하세요: " BMPW
-    ./PINGPONG config set --blockmesh.email="$BMEMAIL" --blockmesh.pwd="$BMPW"
-    ./PINGPONG stop --depins=blockmesh
-    ./PINGPONG start --depins=blockmesh
-    echo ""
-
-    echo -e "${BOLD}5. DAWN 노드 설정 중...${NC}"
-    echo -e "${YELLOW}크롬 익스텐션을 다운하여 회원가입을 진행하세요: Ref=i4r46rfl${NC}"
-    read -p "회원가입을 진행한 Email을 적으세요: " DWEMAIL
-    read -p "패스워드를 입력하세요: " DWPW
-    ./PINGPONG config set --dawn.email="$DWEMAIL" --dawn.pwd="$DWPW"
-    ./PINGPONG stop --depins=dawn
-    ./PINGPONG start --depins=dawn
+# 도커 설치 확인
+echo -e "${BOLD}${CYAN}Docker 설치 확인 중...${NC}"
+if ! command_exists docker; then
+    echo -e "${RED}Docker가 설치되어 있지 않습니다. Docker를 설치하는 중입니다...${NC}"
+    sudo apt update && sudo apt install -y curl net-tools
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    echo -e "${GREEN}Docker가 성공적으로 설치되었습니다.${NC}"
+else
+    echo -e "${GREEN}Docker가 이미 설치되어 있습니다.${NC}"
 fi
 
-echo -e "${GREEN}모든 작업이 완료되었습니다. https://app.pingpong.build/mining/devices 에서 확인하세요${NC}"
+# 필수 패키지 설치
+echo -e "${BOLD}${CYAN}필수 패키지 설치 중...${NC}"
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install --no-install-recommends ubuntu-desktop
+sudo apt-get -y install indicator-appmenu-tools
+sudo apt-get install indicator-session
+sudo apt-get install indicator-datetime
+
+# Nomachine 원격 프로그램 설치
+echo -e "${BOLD}${CYAN}원격프로그램 설치 중...${NC}"
+sudo apt -y install wget
+wget https://download.nomachine.com/download/8.13/Linux/nomachine_8.13.1_1_amd64.deb
+sudo dpkg -i nomachine_8.13.1_1_amd64.deb
+
+# 크롬설치
+echo -e "${BOLD}${CYAN}크롬 설치 중...${NC}"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install fonts-liberation libvulkan1 xdg-utils
+sudo dpkg -i ./google-chrome-stable_current_amd64.deb
+
+# 네트워크 오픈
+echo -e "${BOLD}${CYAN}필요한 네트워크를 오픈 중...${NC}"
+sudo ufw enable
+sudo ufw allow ssh
+sudo ufw allow 22
+sudo ufw allow 4001
+sudo ufw allow 4000/tcp
+
+# dats디앱설치
+echo -e "${BOLD}${CYAN}Dats프로잭트 디앱 설치 중...${NC}"
+wget https://dl.datsproject.io/evm-linux-deb -O evm-linux.deb
+sudo dpkg -i evm-linux.deb
+
+# 데스크탑-원격 프로그램 설치
+echo -e "${YELLOW}이제부터는 안내문구에 따라 데스크탑에서 작업을 진행하세요.${NC}"
+echo -e "${YELLOW}해당 사이트에 방문하시고 원격 프로그램을 데스크탑에 설치해주세요.${NC}"
+echo -e "${YELLOW}https://www.nomachine.com/${NC}"
+echo -e "${YELLOW}해당사이트에 접속하여 Faucet을 받아주세요.: https://faucet.datsproject.io/${NC}"
+read -p "설치 및 Faucet이 끝나면 엔터를 눌러주세요."
+
+# 데스크탑-원격 프로그램 실행
+echo -e "${YELLOW}Nomachine 원격프로그램을 실행한 후 VPS IP와 Port넘버를 기입하여 접속하세요.${NC}"
+echo -e "${YELLOW}해당 정보들을 까먹으셨다면 Contabo로 접속하여 본인의 VPS IP와 Port넘버를 확인할 수 있습니다.${NC}"
+read -p "접속이 끝나면 엔터를 눌러주세요."
+
+# 데스크탑-CMD 실행
+echo -e "${YELLOW}ID에는 root를 PW에는 VPS접속할 때 사용하는 PW를 적고 VPS에 원격으로 접속하세요.${NC}"
+echo -e "${YELLOW}CMD창을 열어서 해당 명령어를 작성하세요: dats-desktop --no-sandbox${NC}"
+read -p "실행이 완료되면 엔터를 눌러주세요."
+
+# 데스크탑-메타마스크 연동
+echo -e "${YELLOW}Dats-project가 실행되면 모바일 메타마스크로 연동을 하세요.${NC}"
+echo -e "${YELLOW}DDOS TEST에 있는 Run speed TEST는 절대 클릭하지 마세요.${NC}"
+echo -e "${YELLOW}DDOS Testnet부터 Blockchain Security까지 모두 활성화를 시키시고 Save Changes를 눌러주세요.${NC}"
+read -p "실행이 완료되면 엔터를 눌러주세요."
+
+echo -e "${GREEN}모든 작업이 완료되었습니다. https://fairdrop.datsproject.io/missions 에서 추가 에어드랍작업을 진행해주세요.${NC}"
 echo -e "${GREEN}스크립트 작성자: https://t.me/kjkresearch${NC}"
